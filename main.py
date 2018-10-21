15cbef8550e501d16e64fb331b42e3e0da84f5d6
Excidion <cedric.wilting@stud.uni-due.de>
2018-10-21 15:34:36 +0200

    Generate messages from ladder changes.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import configparser
 import time
 import os
 
-from utils import load_data, load_medal_times, get_last_SQL_update
+from utils import load_data, load_medal_times, get_last_SQL_update, get_player_name
 from calculations import calculate_complete_data, get_standings
 from plots import plot_total_standings, timedelta_to_string
 from telegram_bot import TelegramBot
@@ -15,9 +15,51 @@ def renew_plot(data):
     plot_total_standings(data, f"Meisterschaftsstand_y{year}_w{week}")
 
 def compare_data_and_create_info_messages(old_data):
-    messages = []
     new_data = calculate_complete_data()
-    new_entries = new_data[~new_data.isin(old_data)].dropna()
+    messages = []
+    messages += info_about_new_times(old_data, new_data)
+    messages += info_about_current_weeks_ladder_changes(old_data, new_data)
+    return messages
+
+
+def info_about_current_weeks_ladder_changes(old_data, new_data):
+    messages = []
+
+    current_track = new_data.loc[new_data["Date"] == new_data["Date"].dropna().max(), "Track"].values[0]
+    new_data = new_data[new_data["Track"] == current_track]
+    old_data = old_data[old_data["Track"] == current_track]
+
+    new_ladder = get_standings(new_data)
+    old_ladder = get_standings(old_data)
+
+    try:
+        changes = new_ladder.index != old_ladder.index
+    except ValueError: # a new player is in the database.
+        new_players = list(set(new_ladder.index) - set(old_ladder.index))
+        new_ladder = new_ladder.loc[~new_ladder.index.isin(new_players)] # ignore his first entries.
+        changes = new_ladder.index != old_ladder.index
+
+    new_ladder = new_ladder[changes].reset_index().reset_index().set_index("Player")
+    old_ladder = old_ladder[changes].reset_index().reset_index().set_index("Player")
+    new_ladder["index_change"] = new_ladder["index"] - old_ladder["index"]
+
+    for player in new_ladder.index.values:
+        overtakes = new_ladder.loc[player, "index_change"]
+        if overtakes > 0:
+            index = new_ladder.loc[player, "index"]
+            overtook = new_ladder[(new_ladder["index"] >= index-overtakes) & (new_ladder["index"] < index)].index.values
+            have_scored = old_data.loc[old_data["Origin"] == "Player", "Player"].unique()
+            overtook = [get_player_name(p) for p in overtook if p in have_scored]
+            new_record = new_data.groupby(["Player", "Track"])["Time"].min().loc[player, current_track]
+            messages.append(f"{get_player_name(player)} scored a {timedelta_to_string(new_record)} and overtook { ', '.join(overtook)}.")
+
+    return messages
+
+
+def info_about_new_times(old_data, new_data):
+    messages = []
+    new_entries_index = new_data[~new_data.isin(old_data)].dropna(how="all").index
+    new_entries = new_data.loc[new_entries_index]
     for row_index, entry in new_entries.iterrows():
         player_name = entry["Player"]
         new_record = timedelta_to_string(entry["Time"])
