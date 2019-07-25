f11e8bf84ac4bdd710b60d52e2079505988d1e00
Excidion <36995046+Excidion@users.noreply.github.com>
2019-07-25 11:19:11 +0200

    removed jupyter kernel installation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 python3 -m venv ../ENV
 source ../ENV/bin/activate
 pip install --upgrade pip
-pip install jupyter
-python -m ipykernel install --user --name $1 --display-name "Python3 ($1)"
 pip install -r requirements.txt
-jupyter kernelspec list
 python --version
