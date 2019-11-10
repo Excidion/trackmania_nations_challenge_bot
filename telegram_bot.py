6d3522df7966b61286bcfb6b40a6d991d6179707
Cedric <cedric.wilting@stud.uni-due.de>
2019-11-10 09:51:48 +0100

    reworked bot for new api

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,14 +9,15 @@ from utils import get_player_name, set_account_to_player_mapping
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-GROUPCHAT_ID = config["TELEGRAM_BOT"]["GROUPCHAT_ID"]
+GROUPCHAT_ID = config.get("TELEGRAM_BOT", "GROUPCHAT_ID")
 
 OPENING_MESSAGE = "\n".join([
     "Hello and welcome to our weekly TrackMania Nations Challenge!",
     "I manage most of what is happening on and around the racetrack.\n",
     "There are multiple ways in which I can help you:",
     "Type /commands for a list of commands that I understand.",
-    "If you are new to the competition I recommend starting with /register so I know who you are (in game)!"])
+    "If you are new to the competition I recommend starting with /register so I know who you are (in game)!"
+])
 
 
 
@@ -30,8 +31,7 @@ class ConversationNotInGroupChatFilter(BaseFilter):
     def filter(self, message):
         chat_id = message.chat.id
         if int(chat_id) == int(GROUPCHAT_ID):
-            message.chat.bot.send_message(chat_id = chat_id,
-                                          text = "I will not do this in public, contact me in a private message.")
+            message.reply_text("I will not do this in public, contact me in a private message.")
             return False # don't handle this conversation in public chat
         else:
             return True
@@ -41,55 +41,66 @@ class ConversationNotInGroupChatFilter(BaseFilter):
 
 class TelegramBot():
     def __init__(self):
-        # bot setup
-        self.updater = Updater(token = config["TELEGRAM_BOT"]["TOKEN"])
-        self.jobs = self.updater.job_queue
+        self.updater = Updater(
+            token = config.get("TELEGRAM_BOT", "TOKEN"),
+            use_context = True,
+        )
         dispatcher = self.updater.dispatcher
 
         # welcome action
-        dispatcher.add_handler(MessageHandler(
-            Filters.status_update.new_chat_members,
-            self.welcome_action))
+        dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, self.welcome_action))
 
         # simple commands
-        COMMAND_MAP = {"chat_id": self.print_chat_id,
-                       "ladder": self.print_ladder,
-                       "week": self.print_ladder,
-                       "total": self.print_plot_link,
-                       "graph": self.print_plot_link,
-                       "link": self.print_website_link}
-        PRIVATE_COMMAND_MAP = {"start": self.help,
-                               "help": self.help,
-                               "commands": self.print_commands,
-                               "server": self.print_join_instructions}
-
+        COMMAND_MAP = {
+            "chat_id": self.print_chat_id,
+            "ladder": self.print_ladder,
+            "week": self.print_ladder,
+            "total": self.print_plot_link,
+            "graph": self.print_plot_link,
+            "link": self.print_website_link,
+        }
+        PRIVATE_COMMAND_MAP = {
+            "start": self.help,
+            "help": self.help,
+            "commands": self.print_commands,
+            "server": self.print_join_instructions,
+        }
 
         for command in COMMAND_MAP:
-            dispatcher.add_handler(CommandHandler(command,
-                                                  COMMAND_MAP[command],
-                                                  filters = UserInGroupChatFilter()))
+            dispatcher.add_handler(
+                CommandHandler(
+                    command,
+                    COMMAND_MAP[command],
+                    filters = UserInGroupChatFilter(),
+                )
+            )
+
         for command in PRIVATE_COMMAND_MAP:
-            dispatcher.add_handler(CommandHandler(command,
-                                                  PRIVATE_COMMAND_MAP[command],
-                                                  filters = (UserInGroupChatFilter() and
-                                                  ConversationNotInGroupChatFilter())))
+            dispatcher.add_handler(
+                CommandHandler(
+                    command,
+                    PRIVATE_COMMAND_MAP[command],
+                    filters = UserInGroupChatFilter() & ConversationNotInGroupChatFilter(),
+                )
+            )
 
 
         # advanced commands
         dispatcher.add_handler(ConversationHandler(
-            entry_points = [CommandHandler("register", self.start_registration,
-                                           filters = (UserInGroupChatFilter() and
-                                                      ConversationNotInGroupChatFilter()))],
-            states = {0: [MessageHandler(Filters.text,
-                                         self.store_name,
-                                         pass_user_data=True)],
-                      1: [MessageHandler(Filters.text,
-                                         self.store_account,
-                                         pass_user_data=True)],
-                      2: [MessageHandler(Filters.text,
-                                       self.bool_decision_to_save,
-                                       pass_user_data=True)]},
-            fallbacks = [CommandHandler("cancel", self.cancel)]))
+            entry_points = [
+                CommandHandler(
+                    "register",
+                    self.start_registration,
+                    filters = UserInGroupChatFilter() & ConversationNotInGroupChatFilter(),
+                )
+            ],
+            states = {
+                0: [MessageHandler(Filters.text, self.store_name)],
+                1: [MessageHandler(Filters.text, self.store_account)],
+                2: [MessageHandler(Filters.text, self.bool_decision_to_save)]
+            },
+            fallbacks = [CommandHandler("cancel", self.cancel)]),
+        )
 
 
 
@@ -114,120 +125,138 @@ class TelegramBot():
         self.updater.bot.send_message(chat_id=GROUPCHAT_ID, text=text)
 
     def send_results_to_groupchat(self):
-        webserver = config["DATA_SOURCES"]["WEBSERVER"]
-        filename = config["SAVE_POINTS"]["CURRENT_PLOT"]
+        webserver = config.get("DATA_SOURCES", "WEBSERVER")
+        filename = config.get("SAVE_POINTS", "CURRENT_PLOT")
         ts = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # timestamp to avoid using cached old thumbnails
-
-        self.updater.bot.send_message(chat_id = GROUPCHAT_ID,
-                                      text = "Here are this weeks results!")
-        self.updater.bot.send_message(chat_id = GROUPCHAT_ID,
-                                      text = get_ladder_as_html(),
-                                      parse_mode = "HTML")
-        self.updater.bot.send_message(chat_id = GROUPCHAT_ID,
-                                      text = "And this is the influence on the total rankings:")
-        self.updater.bot.send_photo(chat_id = GROUPCHAT_ID,
-                                    photo = f"https://{webserver}/{filename}.png?a={ts}")
+        self.updater.bot.send_message(
+            chat_id = GROUPCHAT_ID,
+            text = "Here are this weeks results!",
+        )
+        self.updater.bot.send_message(
+            chat_id = GROUPCHAT_ID,
+            text = get_ladder_as_html(),
+            parse_mode = "HTML",
+        )
+        self.updater.bot.send_message(
+            chat_id = GROUPCHAT_ID,
+            text = "And this is the influence on the total rankings:",
+        )
+        self.updater.bot.send_photo(
+            chat_id = GROUPCHAT_ID,
+            photo = f"https://{webserver}/{filename}.png?a={ts}",
+        )
         print("Posted results to groupchat.")
 
 
     # simple commands
-    def help(self, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text=OPENING_MESSAGE)
+    def help(self, update, context):
+        update.message.reply_text(OPENING_MESSAGE)
 
-    def print_chat_id(self, bot, update):
-        chat_id = update.message.chat_id
-        bot.send_message(chat_id = chat_id, text = chat_id)
+    def print_chat_id(self, update, context):
+        update.message.reply_text(str(update.message.chat_id))
 
-    def print_plot_link(self, bot, update):
-        webserver = config["DATA_SOURCES"]["WEBSERVER"]
-        filename = config["SAVE_POINTS"]["CURRENT_PLOT"]
+    def print_plot_link(self, update, context):
+        webserver = config.get("DATA_SOURCES", "WEBSERVER")
+        filename = config.get("SAVE_POINTS", "CURRENT_PLOT")
         ts = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # timestamp to avoid using cached old thumbnails
-        bot.send_photo(chat_id = update.message.chat_id,
-                       photo = f"https://{webserver}/{filename}.png?a={ts}")
+        update.message.reply_photo(photo=f"https://{webserver}/{filename}.png?a={ts}")
 
-    def print_website_link(self, bot, update):
-        webserver = config["DATA_SOURCES"]["WEBSERVER"]
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = f"https://{webserver}")
+    def print_website_link(self, update, context):
+        webserver = config.get("DATA_SOURCES", "WEBSERVER")
+        update.message.reply_text(f"https://{webserver}")
 
-    def print_join_instructions(self, bot, update):
-        webserver = config["DATA_SOURCES"]["WEBSERVER"]
-        server_name = config["DATA_SOURCES"]["TM_SERVER_NAME"]
-        pwd = config["DATA_SOURCES"]["TM_SERVER_PWD"]
+    def print_join_instructions(self, update, context):
+        webserver = config.get("DATA_SOURCES", "WEBSERVER")
+        server_name = config.get("DATA_SOURCES", "TM_SERVER_NAME")
+        pwd = config.get("DATA_SOURCES", "TM_SERVER_PWD")
         id = update.message.chat_id
-        bot.send_message(chat_id=id, text="To join the server you have to enter the following link to TrackMania internal browser:")
-        bot.send_message(chat_id=id, text=f"tmtp://#addfavourite={server_name}")
-        bot.send_photo(chat_id=id, photo=f"https://{webserver}/tm_browser.png")
-        bot.send_photo(chat_id=id, photo=f"https://{webserver}/tm_addfavo.png")
-        bot.send_message(chat_id=id, text="This will add the server to your list of favourites.")
-        bot.send_message(chat_id=id, text=f"The servers password is \"{pwd}\".")
+        update.message.reply_text("To join the server you have to enter the following link to TrackMania internal browser:")
+        update.message.reply_text(f"tmtp://#addfavourite={server_name}")
+        update.message.reply_photo(photo=f"https://{webserver}/tm_browser.png")
+        update.message.reply_photo(photo=f"https://{webserver}/tm_addfavo.png")
+        update.message.reply_text("This will add the server to your list of favourites.")
+        update.message.reply_text(f"The servers password is \"{pwd}\".")
 
-    def print_commands(self, bot, update):
-        message = "\n".join(["These are the commands I know and what they do:",
-                             "/week - Shows this weeks rankings.",
-                             "/total - Shows the graph of the total season rankings.",
-                             "/link - Shows the link to the website.",
-                             "\nThe following commands can just be handled in private messages with me:",
-                             "/server - Shows you how to connect to the game server.",
-                             "/register - Make your name appear in the rankings. Recommended, if you haven't done this yet."])
+    def print_commands(self, update, context):
+        update.message.reply_text(
+            "\n".join([
+                "These are the commands I know and what they do:",
+                "/week - Shows this weeks rankings.",
+                "/total - Shows the graph of the total season rankings.",
+                "/link - Shows the link to the website.",
+                "\nThe following commands can just be handled in private messages with me:",
+                "/server - Shows you how to connect to the game server.",
+                "/register - Make your name appear in the rankings. Recommended, if you haven't done this yet."
+            ])
+        )
 
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = message)
-
-
-    def print_ladder(self, bot, update):
-        message = get_ladder_as_html()
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = message,
-                         parse_mode = "HTML")
+    def print_ladder(self, update, context):
+        update.message.reply_text(get_ladder_as_html(), parse_mode="HTML")
 
 
     # commands for advanced conversations
-    def start_registration(self, bot, update):
+    def start_registration(self, update, context):
         user_account = update.message.from_user["username"]
         first_name = update.message.from_user["first_name"]
         print(f"{user_account} ({first_name}) started registration.")
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = "\n".join(["I will take you through the registration process. You can abort the process anytime via /cancel ",
-                                           "\nTo get started I need your first name.",
-                                           "This name will be displayed on all rankings and graphs."]))
+        update.message.reply_text(
+            text = "\n".join([
+                "I will take you through the registration process. You can abort the process anytime via /cancel ",
+                "\nTo get started I need your first name.",
+                "This name will be displayed on all rankings and graphs.",
+            ])
+        )
         return 0
 
-    def store_name(self, bot, update, user_data):
-        webserver = config["DATA_SOURCES"]["WEBSERVER"]
-        user_data["name"] = update.message.text
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = "\n".join(["Next up I need your TrackMania account name.",
-                                           "You can see your account name when logging into TrackMania."]))
-        bot.send_photo(chat_id = update.message.chat_id,
-                       photo = f"https://{webserver}/register_instruction.png")
+    def store_name(self, update, context):
+        webserver = config.get("DATA_SOURCES", "WEBSERVER")
+        context.user_data["name"] = update.message.text
+        update.message.reply_text(
+            text = "Next up I need your TrackMania account name.\nYou can see your account name when logging into TrackMania.",
+        )
+        try:
+            update.message.reply_photo(photo=f"https://{webserver}/register_instruction.png")
+        except: pass
         return 1
 
-    def store_account(self, bot, update, user_data):
+    def store_account(self, update, context):
         account = update.message.text
-        user_data["account"] = account
-        name = user_data["name"]
-        bot.send_message(chat_id = update.message.chat_id,
-                         text = f"I will now link the TrackMania account \"{account}\" to \"{name}\".\nDo you want to proceed?",
-                         reply_markup = ReplyKeyboardMarkup([["Yes", "No"]], one_time_keyboard=True))
-
+        context.user_data["account"] = account
+        name = context.user_data["name"]
+        update.message.reply_text(
+            f"I will now link the TrackMania account \"{account}\" to \"{name}\".\nDo you want to proceed?",
+            reply_markup = ReplyKeyboardMarkup([["Yes", "No"]], one_time_keyboard=True),
+        )
         return 2
 
-    def bool_decision_to_save(self, bot, update, user_data):
+    def bool_decision_to_save(self, update, context):
         reply = update.message.text
         if reply.lower() == "yes":
-            account = user_data["account"]
-            name =  user_data["name"]
+            account = context.user_data["account"]
+            name =  context.user_data["name"]
             set_account_to_player_mapping(account, name)
-            update.message.reply_text(f"TM-Account \"{account}\" has been mapped to \"{name}\".",
-                                      reply_markup=ReplyKeyboardRemove())
+            update.message.reply_text(
+                f"TM-Account \"{account}\" has been mapped to \"{name}\".",
+                reply_markup = ReplyKeyboardRemove(),
+            )
         else:
-            update.message.reply_text("Action canceled.",
-                                      reply_markup=ReplyKeyboardRemove())
+            update.message.reply_text("Action canceled.", reply_markup=ReplyKeyboardRemove())
         return ConversationHandler.END
 
 
-
-    def cancel(self, bot, update):
+    def cancel(self, update, context):
         update.message.reply_text("Action canceled.")
         return ConversationHandler.END
+
+
+
+
+if __name__ == '__main__':
+
+    chatbot = TelegramBot()
+    chatbot.start_bot()
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt: pass
+    chatbot.stop_bot()
